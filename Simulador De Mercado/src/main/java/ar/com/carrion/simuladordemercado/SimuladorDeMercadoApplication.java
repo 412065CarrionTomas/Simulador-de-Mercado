5cb7b45cb99865a08669bd169e7a63e792891930
Carrion Tomas <carriontas@gmail.com>
2025-12-10 01:05:44 -0300

    first commit

---
+++ Simulador De Mercado/src/main/java/ar/com/carrion/simuladordemercado/SimuladorDeMercadoApplication.java
@@ -0,0 +1,13 @@
+package ar.com.carrion.simuladordemercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorDeMercadoApplication {
+
+	 static void main(String[] args) {
+		SpringApplication.run(SimuladorDeMercadoApplication.class, args);
+	}
+
+}
---
+++ Simulador De Mercado/src/test/java/ar/com/carrion/simuladordemercado/SimuladorDeMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.carrion.simuladordemercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorDeMercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
